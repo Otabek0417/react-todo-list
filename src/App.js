ce610d90c98cc7c175586e52ac2311ee25917853
Otabek0417 <otabeksodiqov654@gmail.com>
2023-03-19 14:16:10 +0500

    hello react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  const updateData = (id) => {
+    const newData = data.map((item) => {
+      if (item.id === id) {
+        return { ...item, selected: !item.selected };
+      }
+      return item;
+    });
+    setData(newData);
+  };
+
+  const deleteItem = (id) => {
+    const newData = data.filter((item) => {
+      return item.id !== id;
+    });
+    setData(newData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="title">Daily To Do List</h1>
+        <Form setData={setData} />
+        <TodoList data={data} updateData={updateData} deleteItem={deleteItem} />
+        <hr />
+        <footer>
+          <p>Items: {data.length}</p>
+
+          <button onClick={() => {
+            setData([])
+          }}>Clear all</button>
+        </footer>
+      </div>
     </div>
   );
 }
